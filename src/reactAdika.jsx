9b6616ef14b49399649a6040caa94e77ba4253b1
Adika <hidayatadika@gmail.com>
2025-09-15 19:21:32 +0700

    test react js adika

---
+++ src/reactAdika.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./cssAdika.css";
+
+export default function ReactAdika() {
+  return (
+    <div className="container-adika">
+      <h1 className="hello-title">Hello World</h1>
+    </div>
+  );
+}
