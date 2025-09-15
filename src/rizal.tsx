2a2aecd50fbc6d7e9cd75ef53f07704d3a44460c
Rizal <rizalmaulanaairlanggad4itb2024@gmail.com>
2025-09-15 19:14:53 +0700

    rizal

---
+++ src/rizal.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Rizal extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Rizal Component</h1>
+                <p>Hello World</p>
+            </div>
+        );
+
+    }
+}
