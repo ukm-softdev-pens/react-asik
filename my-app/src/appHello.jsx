c6acf974a6d844ca923f4129828e691c0b50a712
Dechiviel <farissoehartono@gmail.com>
2025-09-15 19:06:47 +0700

    gtw, aku buat sesimple mungkin

---
+++ my-app/src/appHello.jsx
@@ -0,0 +1,13 @@
+import './appHello.css'
+
+function App() {
+    return (
+        <>
+            <h1 className='app-title'>Hello, world!</h1>
+            <p className='app-description'>This is a simple React application I built in under 5 minutes (probably).</p>
+            
+        </>
+    );
+}
+
+export default App
