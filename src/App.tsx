e2211ad0d4ec3dcb36c37eba33d82fbc6e029aae
Rafi K Yogananda <rafiyogananda9@gmail.com>
2025-09-15 20:23:32 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import NavbarPage from './components/navbar'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <NavbarPage/>
-        <h1>Hello World</h1>
-        <p>saya baru belajar react tsx</p>
-    
-      </div>  
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> Don't be a dumb
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
