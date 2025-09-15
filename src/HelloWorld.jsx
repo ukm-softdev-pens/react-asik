234ba405dff7bb746a62dcd7d0b639d9a6a2ec20
Naufal Rizqulloh <nopalriz1225@gmail.com>
2025-09-15 19:20:55 +0700

    punya naufal

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,10 @@
+// src/HelloWorld.jsx
+import { Link } from "react-router-dom";
+
+export default function HelloWorld() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
