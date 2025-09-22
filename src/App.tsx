a4eb6a927e07718e2af648c71f0f6f6e4ea86f00
yowxy <iklilblender@gmail.com>
2025-09-22 18:54:40 +0700

    feat: install tailwind & create Simple Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react'
 import './App.css'
-import NavbarPage from './components/navbar'
+import './global.css'
 
 function App() {
+
+  const [count,setCount] = useState(0)
 
   return (
     <>
       <div>
-        <NavbarPage/>
-        <h1>Hello World</h1>
-        <p>saya baru belajar react tsx</p>
-    
+        <h1 className='font-bold'>jumlah count {count}</h1>
+        <p className='mb-4'>simple Router</p>
+        <div className='flex flex-row justify-center gap-4'>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        </div>
       </div>  
     </>
   )
